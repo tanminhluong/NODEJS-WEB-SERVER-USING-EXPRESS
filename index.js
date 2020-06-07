cdd534001ca5e847192e58c31f6ddaca63aa724a
tan minh <tanminhluong99@gmail.com>
2020-06-07 10:49:20 +0700

    enhancing code...

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.use(express.static('public'));
+
+
 app.get('/', function(req, res){
 	res.render('index', {
 		name: 'Tan Minh'
