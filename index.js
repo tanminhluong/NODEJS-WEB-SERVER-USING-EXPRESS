dde14d91400d5ea4d5e7d0c50c5b497accfa3ebd
tan minh <tanminhluong99@gmail.com>
2020-06-07 11:41:43 +0700

    validate and cookie lesson

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var port = 3000;
 var userRoute = require('./routes/user.route');
@@ -10,7 +11,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(cookieParser());
 app.use(express.static('public'));
 
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,10 +2,16 @@ var express = require('express');
 var shortid = require('shortid');
 
 var db = require('../db');
-var controller = require('../controllers/user.controller.js')
+var controller = require('../controllers/user.controller.js');
+var validate = require('../validate/user.validate.js');
 
 var router = express.Router();
 router.get('/', controller.index);
+
+router.get('/cookie', function(req, res , next){
+	res.cookie('user-id', 12345);
+	res.send('Hello');
+});
 
 router.get('/search', controller.search);
 
@@ -13,6 +19,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
